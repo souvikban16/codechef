618da96e137036453caed94970baf95df3d38ef2
souvikban16 <59077658+souvikban16@users.noreply.github.com>
2020-02-10 01:32:24 +0530

    f

---
+++ constraints.py
@@ -0,0 +1,9 @@
+
+while True:
+    try:
+        number1 = int(input('Number1: '))
+        if number1 < 1 or number1 > 10:
+            raise ValueError
+        break
+    except ValueError:
+        pass
