4f78bf2580907ddd3c4a1ab0fd5690c8386fc4d2
souvikban16 <59077658+souvikban16@users.noreply.github.com>
2020-02-22 14:14:00 +0530

    Update CMPRSS.py

--- CMPRSS.py
+++ CMPRSS.py
@@ -1,14 +1,33 @@
 for t in range(int(input())):
-	a=int(input())
-	n=list(map(int,input().split()))
-	s=''
-	index=0
-	subsets=[]
-	a,b=0,0
-	for i in range(len(n)-1):
-		if n[i]+1==n[i+1]:
-			k=0
-			while n[i]+k==n[i+k] and k<len(n):
-				subsets.append(n[i])
-				k+=1
-	print(subsets)
+	n=int(input())
+	ls=list(map(int,input().split()))
+	ls.append(44)
+	st=[]
+	s=""
+
+	counter=1
+	start=ls[0]
+
+	for i in range(1,n+1):
+#		print("element=",ls[i]," counter=",counter," start=",start)
+		if start+counter!=ls[i]:
+#			print("does not comply")
+			if counter!=1:
+				st.append(ls[ls.index(start):i])
+				if counter>2:
+					s+=str(start)+"..."+str(ls[i-1])+","
+				else:
+					s+=str(start)+","+str(ls[i-1])+","
+				start=ls[i]
+#				print("start=",start)
+				counter=1
+				continue
+			else:
+				st.append(start)
+				s+=","+str(start)
+				start=ls[i]				
+				counter=1
+
+		counter+=1
+#	print(st)
+	print(s)
